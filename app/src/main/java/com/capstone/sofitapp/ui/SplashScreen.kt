ea2cdb5fa6a2735201b061ebf8eb0614ec203789
Husni Sabar <husnisabar592@gmail.com>
2023-05-22 11:13:05 +0700

    menambahkan splash screen

---
+++ app/src/main/java/com/capstone/sofitapp/ui/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.capstone.sofitapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.sofitapp.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
