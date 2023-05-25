c4c641990b76cf3fd35875df798db8fe0939fa86
Husni Sabar <husnisabar592@gmail.com>
2023-05-25 17:26:43 +0700

    xml register beres

--- app/src/main/java/com/capstone/sofitapp/ui/SplashScreen.kt
+++ app/src/main/java/com/capstone/sofitapp/ui/SplashScreen.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import com.capstone.sofitapp.databinding.ActivitySplashScreenBinding
+import com.capstone.sofitapp.ui.auth.RegisterActivity
 
 class SplashScreen : AppCompatActivity() {
 
@@ -17,7 +18,7 @@ class SplashScreen : AppCompatActivity() {
         setContentView(binding.root)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, WelcomeActivity::class.java))
+            startActivity(Intent(this, RegisterActivity::class.java))
             finish()
         },duration)
     }
--- app/src/main/java/com/capstone/sofitapp/ui/auth/RegisterActivity.kt
+++ app/src/main/java/com/capstone/sofitapp/ui/auth/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.capstone.sofitapp.ui.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.capstone.sofitapp.databinding.ActivityRegisterBinding
@@ -10,7 +11,17 @@ class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        btnLogin()
+    }
+
+    private fun btnLogin() {
+        binding.tvLogin2.setOnClickListener {
+            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
