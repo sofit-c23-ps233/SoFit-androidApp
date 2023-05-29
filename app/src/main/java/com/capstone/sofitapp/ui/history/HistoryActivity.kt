f92b96f98bd09770c88ed2d123ce339fef5fad12
Husni Sabar <husnisabar592@gmail.com>
2023-05-29 20:28:46 +0700

    menambahkan history activity

--- app/src/main/java/com/capstone/sofitapp/ui/history/HistoryActivity.kt
+++ app/src/main/java/com/capstone/sofitapp/ui/history/HistoryActivity.kt
@@ -1,8 +1,11 @@
 package com.capstone.sofitapp.ui.history
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.capstone.sofitapp.databinding.ActivityHistoryBinding
+import com.capstone.sofitapp.ui.MainActivity
+import com.capstone.sofitapp.ui.WelcomeActivity
 
 class HistoryActivity : AppCompatActivity() {
 
@@ -10,7 +13,19 @@ class HistoryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityHistoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        btnBack()
+    }
+
+    private fun btnBack() {
+        binding.ivBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+        }
     }
 }
